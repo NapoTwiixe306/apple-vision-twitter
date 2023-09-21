d21e158647d8b04477e44e1ed89ee1f340e7f1f3
NapoTwiixe <julien.milants@gmail.com>
2023-09-21 20:31:05 +0200

    inital commit

---
+++ src/components/Rectangle/index.js
@@ -0,0 +1,4 @@
+export {default as Profile} from './Profile';
+export {default as Suggestion} from './Suggestion';
+
+// non utilisé, pour le moment
---
+++ src/views/homePages.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+//import Navbar from "../components/Navbar";
+function HomePages() {
+    return (
+        <>
+            {/*<Navbar/>*/}
+            <div className="firstRectangle">
+                <div className="firstRectangle_content">
+                    <h1>FirstRectangle <br/>(va y avoir le main, et le principal du projet figma)</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default HomePages;
