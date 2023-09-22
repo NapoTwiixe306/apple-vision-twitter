8a385774afad33e24067cbb9d6ed03fa30ba48e2
NapoTwiixe <julien.milants@gmail.com>
2023-09-22 17:40:44 +0200

    add Header with 3 logo and one Input

--- src/views/homePages.jsx
+++ src/views/homePages.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faMicrophone, faDownLeftAndUpRightToCenter, faCog} from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import Navbar from "../components/Navbar";
+
 function HomePages() {
     return (
         <>
             <Navbar/>
             <div className="firstRectangle">
                 <div className="firstRectangle_content">
-                    <h1>Rectangle Principal</h1>
+                    <header className="header">
+                        <section className="nav">
+                            <div className="largeScreen">
+                                <FontAwesomeIcon icon={faDownLeftAndUpRightToCenter} className="logo"/>
+                            </div>
+                            <div className="twitterLogo">
+                                <FontAwesomeIcon icon={faTwitter} className="logo"/>
+                            </div>
+                            <div className="SearchContainer">
+                                <input type="search" placeholder="Search on Twitter" className="Search" />
+                                <FontAwesomeIcon icon={faMicrophone} size="1x" className="SearchIcon" />
+                            </div>
+                            <div className="Settings">
+                                <FontAwesomeIcon icon={faCog}/>
+                            </div>
+                        </section>
+                        <section className="tweet">
+                            Tweet
+                        </section>
+                    </header>
                 </div>
             </div>
         </>
