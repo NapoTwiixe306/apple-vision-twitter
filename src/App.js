a1c1ecf452ae27b8f3bea04c2453790426bc92e8
NapoTwiixe <julien.milants@gmail.com>
2023-09-23 20:20:44 +0200

    ADD Copyright

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
         <Header/>
+        <div className="copyright">
+            <h2><span>Maquettes Figma</span>: <a href="https://twitter.com/ln_dev7" rel="noopener noreferrer" target="_blank">Ln_Dev7</a></h2>
+            <h2><span>Code</span>: <a href="https://twitter.com/JulienMl3" rel="noopener noreferrer" target="_blank">Julien_Mlnts</a></h2>
+        </div>
     </>
   );
 }
