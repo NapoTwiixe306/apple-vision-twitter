0b7c809010887dc54a221a150d6275cfdd0e35fc
NapoTwiixe <julien.milants@gmail.com>
2023-09-23 16:05:49 +0200

    ADD Items on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+
+import Home from '../img/IconsNavb/Home.png'
+import Search from '../img/IconsNavb/Search.png'
+import Bell from '../img/IconsNavb/Bell.png'
+import Messages from '../img/IconsNavb/Messages.png'
+import Favorites from '../img/IconsNavb/Favorites.png'
+import Settings from '../img/IconsNavb/Settings.png'
+
 function Navbar() {
     return (
         <>
             <div className="Navbar">
                 <div className="Navbar_content">
+                    <div className="Home">
+                        <img src={Home} alt=""/>
+                        <p>Home</p>
+                    </div>
+                    <div className="Search">
+                        <img src={Search} alt=""/>
+                        <p>Search</p>
+                    </div>
+                    <div className="Bell">
+                        <img src={Bell} alt=""/>
+                        <p>Bell</p>
+                    </div>
+                    <div className="Message">
+                        <img src={Messages} alt=""/>
+                        <p>Message</p>
+                    </div>
+                    <div className="Favorites">
+                        <img src={Favorites} alt=""/>
+                        <p>Favorites</p>
+                    </div>
+                    <div className="Settings">
+                        <img src={Settings} alt=""/>
+                        <p>Settings</p>
+                    </div>
                 </div>
             </div>
         </>
