c444aa60590a5c3a68b7e6abbeee84729e658a7f
NapoTwiixe <julien.milants@gmail.com>
2023-09-23 13:22:16 +0200

    Add Second Tweet

--- src/views/Tweet.jsx
+++ src/views/Tweet.jsx
@@ -4,6 +4,8 @@ import Comment from '../img/Response/Comment.png'
 import ReTweet from '../img/Response/ReTweet.png'
 import Like from '../img/Response/Like.png'
 import Views from '../img/Response/Views.png'
+import Point from '../img/Icons/Point.png'
+import Illu from '../img/Illustrations.png'
 function Tweet() {
     return (
         <>
@@ -11,7 +13,7 @@ function Tweet() {
                 <div className="PostsOne">
                     <div className="InfosPoster">
                         <div className="Images"></div>
-                        <p className="Poster">
+                        <div className="Poster">
                             <span className="strong">
                                 Twitter
                                  <img src={Certified} alt="" className="Certi"/>
@@ -19,7 +21,12 @@ function Tweet() {
                             <span className="infos">
                                 @Twitter •  9h
                             </span>
-                        </p>
+                            <span className="Point">
+                                <button>
+                                    <img src={Point} alt=""/>
+                                </button>
+                            </span>
+                        </div>
                         <div className="sepa"></div>
                     </div>
                     <div className="TextPoster">
@@ -43,10 +50,46 @@ function Tweet() {
                         <img src={Views} alt=""/>
                     </div>
                 </div>
-                <div className="separator"></div>
+                <div className="separator1"></div>
                 <div className="PostsTwo">
-
+                    <div className="InfosPoster">
+                        <div className="Images"></div>
+                        <div className="Poster">
+                            <span className="strong">
+                                Apple
+                                 <img src={Certified} alt="" className="Certi"/>
+                            </span>
+                            <span className="infos">
+                                @Apple •  8h
+                            </span>
+                            <span className="Point">
+                                <button>
+                                    <img src={Point} alt=""/>
+                                </button>
+                            </span>
+                        </div>
+                        <div className="sepa"></div>
+                    </div>
+                    <div className="TextPoster">
+                        <div className="text">
+                            <div className="img">
+                                <img src={Illu} alt=""/>
+                            </div>
+                            <p>
+                                Say goodbye to prying eyes and hello to secure conversations.
+                                We're giving early access to Encrypted Direct Messages v1 to our verified users.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="icons">
+                        <img src={Comment} alt=""/>
+                        <img src={ReTweet} alt=""/>
+                        <img src={Like} alt=""/>
+                        <img src={Views} alt=""/>
+                    </div>
                 </div>
+                <div className="separator2"></div>
+
             </div>
 
         </>
